b6897baa1e94e6e06b21e75f4bb581e0b8dea66b
Christian Paling <christian.paling@googlemail.com>
2015-03-17 11:26:09 +0100

    implementation of remote object

---
+++ src/rmi/example/RemoteUtilityImpl.java
@@ -0,0 +1,26 @@
+package rmi.example;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.time.ZonedDateTime;
+
+public class RemoteUtilityImpl extends UnicastRemoteObject implements RemoteUtility {
+
+	protected RemoteUtilityImpl() throws RemoteException {
+	}
+
+	@Override
+	public String echo(String msg) {
+		return msg;
+	}
+
+	@Override
+	public ZonedDateTime getServerTime() {
+		return ZonedDateTime.now();
+	}
+
+	@Override
+	public int add(int n1, int n2) {
+		return n1 + n2;
+	}
+}
