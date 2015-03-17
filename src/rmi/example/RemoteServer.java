a6f174706e833d33a896b36c561e4c6ee123da01
Christian Paling <christian.paling@googlemail.com>
2015-03-17 11:42:44 +0100

    server application implemented

---
+++ src/rmi/example/RemoteServer.java
@@ -0,0 +1,31 @@
+package rmi.example;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RemoteServer {
+	public static void main(String[] args) {
+		SecurityManager secManager = System.getSecurityManager();
+		
+		if (secManager == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		try {
+			RemoteUtilityImpl remoteUtility = new RemoteUtilityImpl();
+			
+			// locate the registry
+			Registry registry = LocateRegistry.getRegistry();
+			
+			// bind the exported remote reference in the registry
+			String name = "MyRemoteUtility";
+			registry.rebind(name, remoteUtility);
+			
+			System.out.println("remote server is ready...");
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+}
