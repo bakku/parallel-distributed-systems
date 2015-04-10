2e4ceba629a6f1c9a38c7da5f52e506ba0757f21
Christian Paling <christian.paling@googlemail.com>
2015-04-10 10:26:29 +0200

    fixed exception because of missing throws RemoteException

--- src/rmi/factorial/server/RemoteUtility.java
+++ src/rmi/factorial/server/RemoteUtility.java
@@ -1,8 +1,9 @@
 package rmi.factorial.server;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface RemoteUtility extends Remote {
 	// calculates the factorial of n
-	long factorial(long n);
+	long factorial(long n) throws RemoteException;
 }
